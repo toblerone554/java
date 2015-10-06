b2a2c04f45330bc620029068c5bb690d86e78fc5
toblerone554 <toblerone554@gmail.com>
2015-10-06 22:19:50 +0200

    Probando escapes

---
+++ HolaMundo2.java
@@ -0,0 +1,10 @@
+/*Aunque el compilador importa la librería java.lang completa
+*es conveniente importarla explícitamente por razones didácticas*/
+import java.lang.*;
+public class HolaMundoo {
+String saludo; //La clase Sring la importamos de java.lang
+public void mostrarSaludo(){
+saludo="Hola mundo";
+System.out.println(saludo);//La clase System la importamos de java.lang
+}
+}
---
+++ HolaMundo3.java
@@ -0,0 +1,5 @@
+public class HolaMundo3 {
+	public static void main(String[] args) {
+		 System.out.println("\"Uso de secuencias de escape\"");	
+											}
+}						
