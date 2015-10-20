3172002bd2820efde3d4e3f3f35e0764b48905f6
toblerone554 <toblerone554@gmail.com>
2015-10-20 19:54:01 +0200

    Ejercicios de prueba

---
+++ ProyectoPrueba/src/paquete1/programa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+/**
+ *
+ * @author alonso
+ */
+public class programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hola Mundo");
+        
+        System.out.println("HolaJava");
+        
+        System.out.println("Línea numero 1");
+        System.out.println("");
+        System.out.println("Línea número 2");
+        
+    }
+    
+}
