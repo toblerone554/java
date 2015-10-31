98b1bda8ecd91bc5f27dbf834404fc70976b02dc
toblerone554 <toblerone554@gmail.com>
2015-10-31 17:58:27 +0100

    Ejercicios de pruebas

---
+++ Ejemplo.java
@@ -0,0 +1,16 @@
+public class Ejemplo
+{
+	public static void main(String [] args) 
+	
+	{
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[3]);
+	
+	}
+	
+
+
+
+}
---
+++ TelevisorSony.java
@@ -0,0 +1,15 @@
+public class TelevisorSony
+{
+		public static int pulgadas=29;
+		public static float obtenerprecio()
+		
+		{
+
+			System.out.println("El televisor tiene "+pulgadas);
+		
+		}
+
+
+
+
+}
