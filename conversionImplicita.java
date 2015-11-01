a3b41b3e4211ecf06f9397dd3a45cd119762c688
toblerone554 <toblerone554@gmail.com>
2015-11-01 21:04:22 +0100

    Ejercicio con conversiones de variables implicitas

---
+++ conversionImplicita.java
@@ -0,0 +1,15 @@
+public class conversionImplicita
+{
+	public static void main(String [] args) 
+	
+		{
+			int i;
+			byte b = 30;
+			i=b;
+			
+				System.out.println("La variable i vale "+i);
+			
+		}
+
+
+}
