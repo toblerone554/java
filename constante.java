1fd33a55d83d067b040631af7aaafae5fbf42d17
toblerone554 <toblerone554@gmail.com>
2015-11-02 19:41:38 +0100

    Ejercicios con constantes

---
+++ constante.java
@@ -0,0 +1,23 @@
+public class constante
+
+
+
+{
+
+	
+	public static void main (String[]args)
+	
+		{
+				
+				final double pi=3.1416;
+				double pi2;
+				System.out.println("El valor de pi es\t" +pi);
+		
+				pi2=(double)pi;
+				
+				System.out.println("El valor de pi2 es\t" +pi2);
+		}
+
+
+
+}
