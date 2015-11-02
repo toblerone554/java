89155e633267ab3c3a72b0a56f5b2b7876c18bef
toblerone554 <toblerone554@gmail.com>
2015-11-02 20:49:58 +0100

    Ejercicios con operadores

---
+++ operadores.java
@@ -0,0 +1,54 @@
+public class operadores 
+
+{
+
+	public static void main(String[]args)
+	
+	{
+		int c=4; //Uso de =
+		System.out.println("C vale "+c);
+		
+		System.out.println("SUMA");
+
+		c+=5; // Asi podemos sumar
+		System.out.println("C + 5 vale "+c);
+		c=c+6; // Asi podemos sumar
+		System.out.println("C + 6 vale "+c);
+		
+		System.out.println("RESTA");
+		
+		c-=5; // Asi podemos restar
+		System.out.println("C vale\t"+c);
+		c=c-6; // Asi podemos restar
+		System.out.println("C vale\t"+c);
+		
+		System.out.println("MULTIPLICAR");
+		
+		c*=5; // Asi podemos multiplicar
+		System.out.println("C vale\t"+c);
+		c=c*6; // Asi podemos multiplicar
+		System.out.println("C vale\t"+c);
+		
+		System.out.println("DIVIDIR");
+		
+		c/=8; // Asi podemos dividir
+		System.out.println("C vale\t"+c);
+		c=c/12; // Asi podemos dividir
+		System.out.println("C vale\t"+c);
+	
+		System.out.println("RESTO");
+	
+		c%=5; // Asi podemos sacar resto
+		System.out.println("C vale\t"+c);
+		c=c%6; // Asi podemos sacar resto
+		System.out.println("C vale\t"+c);
+		
+	
+	}
+	
+
+
+	
+	
+	
+}
