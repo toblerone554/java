d7d2800f9782ffeb94e189661bba9d2cd45e3e76
toblerone554 <toblerone554@gmail.com>
2015-11-03 19:26:27 +0100

    Nuevos ejercicios con operadores logicos

---
+++ operadoresLogicos2.java
@@ -0,0 +1,22 @@
+public class operadoresLogicos2
+{
+	public static void main(String[]args)
+	
+		{
+			int p=4;
+			int f=2;
+			
+			if ((p>0)&&(++f>0))
+			{
+				p++;
+				
+			}
+			
+			System.out.println("p vale " +p);
+			System.out.println("f vale " +f);
+		}
+
+
+
+
+}
