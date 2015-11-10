55d18b0405e92afc5ed921ec259d0648365d8f5f
toblerone554 <toblerone554@gmail.com>
2015-11-10 20:58:25 +0100

    Seguimos con instrucciones de control

---
+++ Netbeans/Practicas/src/practicas/UsoIF2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicas;
+
+/**
+ *
+ * @author alonso
+ */
+public class UsoIF2 
+{
+   public static void main(String[]args)
+   {
+     
+      int a =5;
+      
+      if (a % 2 == 0)
+      
+        {
+            System.out.println("Es par");
+        }
+   
+      else 
+        
+       {
+       
+           System.out.println("Es impar");
+       }
+  
+   }
+                    
+}
