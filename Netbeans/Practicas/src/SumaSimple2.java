9cd678983576a40add2d4a5d5c8869cb6bada0d8
toblerone554 <toblerone554@gmail.com>
2016-02-03 21:23:05 +0100

    concatenar con concat

---
+++ Netbeans/Practicas/src/SumaSimple2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alonso
+ */
+public class SumaSimple2 {
+    public static void main (String[] args)
+    
+    {
+        int a=1;
+        System.out.println("el valor de a =".concat(Integer.toString(a)));
+        a=a+10;
+        System.out.println("ahora sumandolo 10 es a =".concat(Integer.toString(a)));
+        
+    }
+    
+}
--- concatenar.java
+++ concatenar.java
@@ -3,9 +3,9 @@ public class concatenar
 		public static void main (String[] args)
 	{
 		int a=1;
-		System.out.println("El valor de a="+a);
+		System.out.println("El valor de a = "+a);
 		a=a+10;
-		System.out.println("ahora sumandole 10 es a="+a);
+		System.out.println("ahora sumandole 10 es a = "+a);
 		
 	}
 	
