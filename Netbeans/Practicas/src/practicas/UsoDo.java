a236ff2198651957bf288af64411d564d683d738
toblerone554 <toblerone554@gmail.com>
2016-02-03 21:03:33 +0100

    concatenar

---
+++ Netbeans/Practicas/src/practicas/UsoDo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicas;
+
+/**
+ *
+ * @author alonso
+ */
+public class UsoDo 
+{
+          public static void main(String[]args)
+          {
+              int a = 0;
+              do 
+              {
+                  System.out.println("El número vale "+a);
+                  a++;
+              }while(a<10);
+
+    
+          }
+}
---
+++ concatenar.java
@@ -0,0 +1,13 @@
+public class concatenar
+{
+		public static void main (String[] args)
+	{
+		int a=1;
+		System.out.println("El valor de a="+a);
+		a=a+10;
+		System.out.println("ahora sumandole 10 es a="+a);
+		
+	}
+	
+	
+}
