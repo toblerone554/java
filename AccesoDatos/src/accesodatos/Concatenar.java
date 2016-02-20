f57042b1a032b8753639b96bd45cc70126ee6565
toblerone554 <toblerone554@gmail.com>
2016-02-20 16:38:24 +0100

    Pruebas de concatenar

---
+++ AccesoDatos/src/accesodatos/Concatenar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesodatos;
+
+/**
+ *
+ * @author alonso
+ */
+public class Concatenar {
+public static void main(String[] args){
+
+    System.out.println("Vamos a sumar dos números");
+    int a=1;
+    System.out.println("El valor de a es = "+a );
+    System.out.println("Vamos a sumar la variable otro número");
+    a=a+2;
+    System.out.println("El valor de a mas 2 es de = "+a);
+}
+}
+
+
---
+++ AccesoDatos/src/accesodatos/HolaMundo2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesodatos;
+
+/**
+ *
+ * @author alonso
+ */
+public class HolaMundo2 {
+public static void main(String[] args) {
+System.out.print("Hola, aquí no hay salto de línea");
+}
+}
